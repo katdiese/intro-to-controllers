66ba2730e1310961cb8263b848ed6ab4a6fca1d5
Kathy Dieser <katdiese@gmail.com>
2016-03-31 14:58:49 -0600

    added calc for seconds in century, controller for exercises

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ var app = angular.module("firstApp", []);
 app.controller("MyFirstController", function($scope) {
   $scope.name = "Severus Snape";
 })
+
+var yearsInCentury = 100
+var daysInYear = 365;
+var hoursInDay = 24;
+var minutesInHour = 60;
+var secondInMinute = 60;
+
+var secondsInCentury = secondInMinute * minutesInHour * hoursInDay * daysInYear * yearsInCentury;
+
+app.controller("ExercisesController", function($scope) {
+  $scope.FavColor = "red";
+  $scope.secondsInCentury = secondsInCentury;
+})
